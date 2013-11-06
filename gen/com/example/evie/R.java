b844a042a46fd96cf1f09b82e2c6f7fbd317c880
Vilcya Wirantana <vwirantana@gmail.com>
2013-11-06 12:35:09 -0500

    BagOfWords

--- gen/com/example/evie/R.java
+++ gen/com/example/evie/R.java
@@ -9,8 +9,8 @@ package com.example.evie;
 
 public final class R {
     public static final class array {
-        public static final int interests_array=0x7f080001;
-        public static final int schools_array=0x7f080000;
+        public static final int interests_array=0x7f090001;
+        public static final int schools_array=0x7f090000;
     }
     public static final class attr {
     }
@@ -21,46 +21,46 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int b_rescan=0x7f0a0008;
-        public static final int button1=0x7f0a001b;
-        public static final int checkBox1=0x7f0a0013;
-        public static final int checkBox2=0x7f0a0014;
-        public static final int checkBox3=0x7f0a0016;
-        public static final int checkBox4=0x7f0a0015;
-        public static final int checkBox5=0x7f0a0018;
-        public static final int checkBox6=0x7f0a0017;
-        public static final int checkBox7=0x7f0a0019;
-        public static final int checkBox8=0x7f0a001a;
-        public static final int editText1=0x7f0a000d;
-        public static final int editText2=0x7f0a000f;
-        public static final int event_icon=0x7f0a001c;
-        public static final int event_name=0x7f0a001d;
-        public static final int gv_main=0x7f0a0004;
-        public static final int iv_event_detail_image=0x7f0a0002;
-        public static final int ll_food_toggle=0x7f0a0005;
-        public static final int relativeLayout1=0x7f0a0009;
-        public static final int spinner1=0x7f0a0011;
-        public static final int tb_free_food=0x7f0a0006;
-        public static final int textView1=0x7f0a000a;
-        public static final int textView2=0x7f0a000b;
-        public static final int textView3=0x7f0a000c;
-        public static final int textView4=0x7f0a000e;
-        public static final int textView5=0x7f0a0012;
-        public static final int textView6=0x7f0a0010;
-        public static final int tv_event_detail_description=0x7f0a0001;
-        public static final int tv_event_detail_name=0x7f0a0000;
-        public static final int tv_free_food_text=0x7f0a0007;
-        public static final int tv_header=0x7f0a0003;
+        public static final int b_rescan=0x7f0b0008;
+        public static final int button1=0x7f0b001b;
+        public static final int checkBox1=0x7f0b0013;
+        public static final int checkBox2=0x7f0b0014;
+        public static final int checkBox3=0x7f0b0016;
+        public static final int checkBox4=0x7f0b0015;
+        public static final int checkBox5=0x7f0b0018;
+        public static final int checkBox6=0x7f0b0017;
+        public static final int checkBox7=0x7f0b0019;
+        public static final int checkBox8=0x7f0b001a;
+        public static final int editText1=0x7f0b000d;
+        public static final int editText2=0x7f0b000f;
+        public static final int event_icon=0x7f0b001c;
+        public static final int event_name=0x7f0b001d;
+        public static final int gv_main=0x7f0b0004;
+        public static final int iv_event_detail_image=0x7f0b0002;
+        public static final int ll_food_toggle=0x7f0b0005;
+        public static final int relativeLayout1=0x7f0b0009;
+        public static final int spinner1=0x7f0b0011;
+        public static final int tb_free_food=0x7f0b0006;
+        public static final int textView1=0x7f0b000a;
+        public static final int textView2=0x7f0b000b;
+        public static final int textView3=0x7f0b000c;
+        public static final int textView4=0x7f0b000e;
+        public static final int textView5=0x7f0b0012;
+        public static final int textView6=0x7f0b0010;
+        public static final int tv_event_detail_description=0x7f0b0001;
+        public static final int tv_event_detail_name=0x7f0b0000;
+        public static final int tv_free_food_text=0x7f0b0007;
+        public static final int tv_header=0x7f0b0003;
     }
     public static final class integer {
-        public static final int message_update_events=0x7f050000;
+        public static final int message_update_events=0x7f060000;
     }
     public static final class layout {
         public static final int activity_event_details=0x7f030000;
@@ -70,39 +70,45 @@ public final class R {
         public static final int event_list_item=0x7f030004;
     }
     public static final class menu {
-        public static final int event_details=0x7f090000;
-        public static final int main=0x7f090001;
-        public static final int sign_up=0x7f090002;
+        public static final int event_details=0x7f0a0000;
+        public static final int main=0x7f0a0001;
+        public static final int sign_up=0x7f0a0002;
+    }
+    public static final class raw {
+        public static final int rawtrainingdata=0x7f040000;
     }
     public static final class string {
-        public static final int academicKey=0x7f060016;
-        public static final int action_settings=0x7f060003;
-        public static final int andrewKey=0x7f06000e;
-        public static final int app_name=0x7f060002;
-        public static final int artsKey=0x7f060014;
-        public static final int collegeKey=0x7f06000f;
-        public static final int culturalKey=0x7f060013;
-        public static final int entertainmentKey=0x7f060012;
-        public static final int event_position=0x7f060000;
-        public static final int foodKey=0x7f060010;
-        public static final int free_food=0x7f060006;
+        public static final int academicKey=0x7f070017;
+        public static final int action_settings=0x7f070003;
+        public static final int andrewKey=0x7f07000f;
+        public static final int app_name=0x7f070002;
+        public static final int artsKey=0x7f070015;
+        public static final int collegeKey=0x7f070010;
+        public static final int culturalKey=0x7f070014;
+        public static final int entertainmentKey=0x7f070013;
+        public static final int event_position=0x7f070000;
+        /**  File I/O 
+         */
+        public static final int filename_trained_data=0x7f07000c;
+        public static final int foodKey=0x7f070011;
+        public static final int free_food=0x7f070006;
         /**  Default string for placeholder purposes 
          */
-        public static final int hello_world=0x7f060001;
-        public static final int nameKey=0x7f06000d;
-        public static final int prefs_file_name=0x7f06000c;
-        public static final int professionalKey=0x7f060017;
-        public static final int serviceKey=0x7f060011;
-        public static final int sportsKey=0x7f060015;
-        public static final int title_activity_event_details=0x7f060004;
-        public static final int title_activity_sign_up=0x7f060005;
-        public static final int wifi_enable_successful=0x7f060009;
+        public static final int hello_world=0x7f070001;
+        public static final int nameKey=0x7f07000e;
+        public static final int prefs_file_name=0x7f07000d;
+        public static final int professionalKey=0x7f070018;
+        public static final int serviceKey=0x7f070012;
+        public static final int sportsKey=0x7f070016;
+        public static final int title_activity_event_details=0x7f070004;
+        public static final int title_activity_sign_up=0x7f070005;
+        public static final int wifi_enable_successful=0x7f070009;
         /**  Wifi updates 
          */
-        public static final int wifi_enable_toast=0x7f060007;
-        public static final int wifi_enable_unsuccessful=0x7f060008;
-        public static final int wifi_scan_successful=0x7f06000a;
-        public static final int wifi_scan_unsuccessful=0x7f06000b;
+        public static final int wifi_enable_toast=0x7f070007;
+        public static final int wifi_enable_unsuccessful=0x7f070008;
+        public static final int wifi_scan_successful=0x7f07000a;
+        public static final int wifi_scan_unsuccessful=0x7f07000b;
     }
     public static final class style {
         /** 
@@ -126,10 +132,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f080001;
     }
 }
--- src/com/smart/evie/BagOfWords.java
+++ src/com/smart/evie/BagOfWords.java
@@ -1,40 +1,95 @@
 package com.smart.evie;
 
+import java.util.ArrayList;
 import java.util.Hashtable;
 
-public class BagOfWords {
-	private static final int CATEGORY_SIZE = 9;
+import com.main.evie.DynamicEventList;
+import com.main.evie.Event;
 
-	private static Hashtable<String, Double> bagOfWords[] = null;
+/**
+ * Bag of Words
+ * 
+ * Example: 
+ * 		BagOfWords bag = new BagOfWords(DynamicEventList);
+ * 		ArrayList<int[]> results = bag.pollWords();
+ * 
+ * Later access of same results (note pollWords must have been called before):
+ * 		ArrayList<int[]> results = bag.getPollResults();
+ * 
+ * @author 
+ *
+ */
+public class BagOfWords {
+	private static final String REGEX_WHITESPACE = "\\s+";
+
+	/** Hashtable that maps words to its index **/
+	private Hashtable<String, Integer> wordIndexMap;
+	
+	/** List of all word pollings across events **/
+	private ArrayList<double[]> allPolls;
+	
+	/** The events instance that is used for allPolls **/
+	private final DynamicEventList events;
+	
+	public BagOfWords(DynamicEventList events) {
+		int wordCount = 0;
+		this.wordIndexMap = new Hashtable<String, Integer>();
+		this.events = events;
+
+		if (events == null) {
+			return;
+		}
+		
+		/* Count words and create mappings of words to indices */
+		for (Event event: events.getEvents()) {
+			/* Tokenize on whitespace */
+			String[] description = event.getDescription().split(REGEX_WHITESPACE);
+			
+			/* Map each word to indices */
+			for (String word: description) {
+				if (!this.wordIndexMap.containsKey(word)) {
+					this.wordIndexMap.put(word, wordCount);
+					wordCount++;
+				}
+			}
+		}
+	}
+
+	/**
+	 * Counts words in each description
+	 * Eventual TODO: eliminate duplicate code in here and constructor
+	 * 
+	 * @return New arraylist - could be empty if no events or no descriptions 
+	 */
+	public ArrayList<double[]> pollWords() {
+		allPolls = new ArrayList<double[]>();
+		
+		/* Run through events and poll for each one */
+		for (Event event: events.getEvents()) {
+			
+			/* Default value on initialization guaranteed to be 0 */
+			double eventWordPoll[] = new double[this.wordIndexMap.size()]; 
+			
+			/* Tokenize on whitespace */
+			String[] description = event.getDescription().split(REGEX_WHITESPACE);
+			
+			for (String word: description) {
+				eventWordPoll[this.wordIndexMap.get(word)]++;
+			}
+
+			allPolls.add(eventWordPoll);
+		}
+
+		return allPolls;
+	}
 	
 	/**
-	 * Initializes structures inside BagOfWords
+	 * Retrieves polling result
+	 *  
+	 * @return null if pollWords has never been called
+	 * 		   ArrayList<int[]> for each word poll in each event
 	 */
-	public void initializeBag() {
-		this.bagOfWords = new Hashtable[CATEGORY_SIZE];
-
-		for (int category = 0; category < CATEGORY_SIZE; category++) {
-			this.bagOfWords[category] = new Hashtable<String, Double>();
-		}
-	}
-
-	private boolean addWord(String word, int category) {
-		if (this.bagOfWords == null) {
-			initializeBag();
-		}
-		
-		if (!categoryValidated(category)) {
-			return false;
-		}
-
-		double probability = 0.5; // INSERT PROBABILITY CALCULATION HERE
-		
-		this.bagOfWords[category].put(word, probability);
-		return true;
-	}
-	
-	
-	private boolean categoryValidated(int category) {
-		return category > 0 && category < CATEGORY_SIZE;
+	public ArrayList<double[]> getPollResult() {
+		return this.allPolls;
 	}
 }
